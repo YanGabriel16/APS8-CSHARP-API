6563a818b8a04cf6d3c09d7ef360f05ea02b739b
YanGabriel16 <yan80ky16@gmail.com>
2023-09-18 19:28:25 -0300

    Adicionar arquivos de projeto.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace APS8_CSHARP_API
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
