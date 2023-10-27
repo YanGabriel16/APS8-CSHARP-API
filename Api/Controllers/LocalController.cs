14f5860933a5b496d48d0c55eb72fe121e905984
Yan Gabriel Silva Queiroz <yan80ky16-gsq@hotmail.com>
2023-10-27 19:25:39 -0300

    Fix :: organizacao de arquivos

---
+++ Api/Controllers/LocalController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APS8_CSHARP_API.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LocalController : ControllerBase
+    {
+        
+    }
+}
